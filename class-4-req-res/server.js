9cbdea733b81bff598df67f8e6081df41858258a
Binay <binayprogramming@gmail.com>
2020-08-01 16:36:15 +0200

    Status code & redirections covered

--- class-4-req-res/server.js
+++ class-4-req-res/server.js
@@ -17,12 +17,19 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.setHeader("Location", "/about");
+      res.statusCode = 301;
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
   }
 
   fs.readFile(`${path}`, (err, data) => {
@@ -30,6 +37,7 @@ const server = http.createServer((req, res) => {
       console.log(err);
       res.end();
     } else {
+      res.statusCode = 200;
       res.end(data);
     }
   });
