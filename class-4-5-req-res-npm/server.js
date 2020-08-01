08aa1b86b11f63c7c1dd38210e95ce725fc50dea
Binay <binayprogramming@gmail.com>
2020-08-01 17:22:48 +0200

    package.json importance, dependecies and more

--- class-4-5-req-res-npm/server.js
+++ class-4-5-req-res-npm/server.js
@@ -1,12 +1,17 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  //lodash
+  const num = _.random(0, 10);
+  console.log(num);
 
   //set header content-type to application/
-
   res.setHeader("Content-Type", "text/html");
+
+  const greet = _.once(() => console.log("hello"));
+  greet();
 
   /* res.write("hello world");
   res.end(); */
